e31d7e8fb6bd16c8cbe85a6d81c8ccbbe8f0c938
Timothy Chin <chinti@bc.edu>
2022-03-30 17:59:49 -0400

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Timothy Chin on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
