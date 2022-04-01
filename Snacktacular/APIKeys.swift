bfa90d773f2beaa30712d0786970ac12c17ccff3
Timothy Chin <chinti@bc.edu>
2022-03-31 23:35:47 -0400

    Section 8.7

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Timothy Chin on 3/31/22.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyDuoJIq0hboJkxtc9aj1NA1Ns_-zXSpnQA"
+}
